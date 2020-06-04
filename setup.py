b12190dbc6840c9fcb652edcbb6720a18a79522c
赵丰 (Zhao Feng) <616545598@qq.com>
2020-06-04 19:07:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md') as fh:
 
 setuptools.setup(
     name='info_cluster',
-    version='0.9',
+    version='0.9.post1',
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'scikit-learn', 'ete3', 'networkx', 'pspartition'],
     author="zhaofeng-shu33",
     author_email="616545598@qq.com",
     description="a hierachical clustering algorithm based on information theory",
-    url="https://github.com/zhaofeng-shu33/info_cluster_python_binding",
+    url="https://github.com/zhaofeng-shu33/info-clustering-python-binding",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     classifiers=[
